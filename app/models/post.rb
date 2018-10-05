e4b5cd56e3f758c403188aff961ee896b7c94ee1
Yann Froment <yann.froment@yaneoweb.com>
2018-10-05 08:17:40 +0200

    annotated Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  date       :string
+#  author     :string
+#  content    :text
+#  file       :string
+#  country    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string
+#  date       :string
+#  author     :string
+#  content    :text
+#  file       :string
+#  country    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
